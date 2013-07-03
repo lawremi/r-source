ce71ba3da047a754a73aa3350caa5b3794bcfc15
Michael Lawrence <michafla@gene.com>
2013-07-02 17:44:12 -0700

    lazy duplicate arguments to list()

--- src/main/builtin.c
+++ src/main/builtin.c
@@ -676,7 +676,7 @@ SEXP attribute_hidden do_makelist(SEXP call, SEXP op, SEXP args, SEXP rho)
 	    SET_STRING_ELT(names, i, R_BlankString);
 	}
 	if (NAMED(CAR(args)))
-	    SET_VECTOR_ELT(list, i, duplicate(CAR(args)));
+	    SET_VECTOR_ELT(list, i, lazy_duplicate(CAR(args)));
 	else
 	    SET_VECTOR_ELT(list, i, CAR(args));
 	args = CDR(args);
